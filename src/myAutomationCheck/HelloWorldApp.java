2edec2ce708bec379fee0c95af44f7712504817f
zamran <family_zj@yahoo.com>
2019-06-03 21:36:02 -0500

    initial commit

---
+++ src/myAutomationCheck/HelloWorldApp.java
@@ -0,0 +1,16 @@
+package myAutomationCheck;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Tutorial\\WebDrivers\\chromedriver.exe");
+
+
+	}
+
+}
